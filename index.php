bdec89577000ff9dd567d9975097f0b568403374
AlcysAlt <awesomealcys@gmail.com>
2022-11-24 12:46:54 +0000

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,30 @@
 <html>
 
 <?php
+// quesry string parameters are accessible through $_GET or $_REQUEST
+switch($_GET['mid']) // Look only at mid parameter. Decide what to do based on value
+{
+    case 1: 
+        $page = 'page1.php'; // Save the redirection to a variable
+        break;
+    case 2:
+        $page = 'page2.php';
+        break;
+    default:
+        $page = 'page3.php';
+        break;
+}
+
+// redirect to correct page
+header('location: '. $page); 
+
 session_start();
 $_SESSION['admin_access']='yes';
 echo 'index page, remote address:';
 echo $_SERVER['REMOTE_ADDR'];
 echo 'server address:';
 echo $_SERVER['SERVER_ADDR'];
-echo file_get_contents("http://bestsiteever/www/admin.php");
+echo file_get_contents("http://bestsiteever.com/www/admin.php");
 /*
 $firstName = 'John';
 $lastName = 'Smith';
