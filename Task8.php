37d7aee616ab510da7e5dac42dfc87b3d0aa26c6
AlcysAlt <awesomealcys@gmail.com>
2022-12-02 17:31:09 +0000

    Update Task8.php

--- Task8.php
+++ Task8.php
@@ -21,20 +21,39 @@
 Class  Flag
 {
     public $name =  'Secret Object';
-    public $flag =  '!YouWerentSuppossedToSeeThisObject!';
     public function getFlag(){
-        echo $flag;
+        global $flag;
+        $flag =  '!YouWerentSuppossedToSeeThisObject!';
+        return $flag;
     }
-}
- 
+};
+function takeData($userInput){
+    return serialize($userInput);
+
+};
+//Data is parsed and used to access the required variable from a deserialized object
+function parseData($input2parse, $requiredVariable){
+
+    $parsedData = unserialize($input2parse);
+
+    if (null !== $parsedData){
+        return eval($parsedData->requiredVariable);
+    }
+    else{
+        return 'Error: No data provided';
+    }
+    
+
+};
  
 //$admin = new ConvisoPerson();
 //$admin_serialize = serialize($admin);
 $flag = new Flag();
 $serialize_flag = serialize($flag);
-echo $serialize_flag;
+//echo $serialize_flag;
 $flag_unserialize = unserialize($serialize_flag);
-echo $flag_unserialize->getFlag();
+//echo $flag_unserialize->getFlag();
+echo $flag_unserialize;
  
  
  
