fc5b823cd900ee5287b2a48e42c0d33b88d95818
AlcysAlt <awesomealcys@gmail.com>
2022-12-07 14:57:48 +0000

    Update account.php

--- account.php
+++ account.php
@@ -18,10 +18,10 @@ if (isset($_POST['funnyButton']) AND $_POST['funnyButton'] == "Funny button that
 // //Checks if the the session variable 'redirected' is set, deny access if empty.
 if (!isset($_SESSION['redirected']) or empty($_SESSION['redirected']) or $_SESSION['redirected'] == '') {
     echo 'Access Denied';
-    exit; 
     //Otherwise if redirected variable is set, empty the variable and display the web page.
   } else if(isset($_SESSION['defaultAccount']) AND $_SESSION['defaultAccount'] == True ){
-    die('!DefaultAccounts=FreeAccess!');
+    echo(var_dump($_SESSION));
+    echo('!DefaultAccounts=FreeAccess!');
     $_SESSION['defaultAccount'] = False;
   } else if ($_SESSION['redirected'] == True){
     $_SESSION['info'] = getAccountInfo();
