bc0c3265bf9a3914c9ace0ad0ac9e92c5fd3a1ff
AlcysAlt <awesomealcys@gmail.com>
2022-12-10 15:29:46 +0000

    Update aboutUs.php

--- aboutUs.php
+++ aboutUs.php
@@ -23,18 +23,22 @@
     <table align="center">
   <tr>
     <td>
+        <h2>CEO: </h2>
       <img src="image1.jpg" alt="Image 1">
       <p>Description for image 1</p>
     </td>
     <td>
+    <h2>Technical Director: </h2>
       <img src="image2.jpg" alt="Image 2">
       <p>Description for image 2</p>
     </td>
     <td>
+    <h2> </h2>
       <img src="image3.jpg" alt="Image 3">
       <p>Description for image 3</p>
     </td>
     <td>
+    <h2>Marketing Manager: </h2>
       <img src="image4.jpg" alt="Image 4">
       <p>Description for image 4</p>
     </td>
