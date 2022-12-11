4ca5892ed040186733bcd6632c6541fc6b965f2b
AlcysAlt <awesomealcys@gmail.com>
2022-12-11 13:53:53 +0000

    Website Finished

--- aboutUs.php
+++ aboutUs.php
@@ -7,44 +7,25 @@
             <th>
             <h1>About Us</h1>
             <h2>Meet the team</h2>
+            <h2>CEO: Jane Johnson</h2>
+      <img src="imgs/red.png" alt="Image 1">
+      <p>Email: jjohnson@bestdomain.biz</p>
+      <p>Best Website Ever is proud to introduce our CEO, who was born and raised in New York and is a lifelong Star Wars fan. With her passion for technology and business, combined with her creative perspective inspired by science fiction, she brings a unique and valuable skillset to our company. As a New York native, she has an in-depth understanding of the city's tech scene and is dedicated to helping Best Website Ever thrive. We are confident in her leadership and are excited for the future of our company. Thank you for considering us for your website needs.</p>
+    
+    <h2>Technical Director: William Thompson</h2>
+      <img src="imgs/green.png" alt="Image 2">
+      <p>Email: wthompson@bestdomain.biz</p>
+      <p>Best Website Ever is pleased to introduce our Technical Director, who was born and raised in Los Angeles. With his passion for technology and expertise in computer science, he brings a valuable perspective to our company. As a native of the city's vibrant tech scene, he is dedicated to helping Best Website Ever thrive. We are confident in his leadership and look forward to a successful future. Thank you for considering us for your website needs.</p>
+
+    <h2>Marketing Manager: Stephen Anderson </h2>
+      <img src="imgs/blue.png" alt="Image 4">
+      <p>Email: sanderson@bestdomain.biz</p>
+      <p>Best Website Ever is pleased to introduce our Marketing Manager, a Cleveland native with a passion for innovation and a strong background in marketing. He brings a valuable perspective to our company and is dedicated to helping us thrive in this market. With his leadership, we are confident in our future success. Thank you for considering us for your website needs.</p>
+      
             </th>
             <th>
         </tr>   
     </table>
-    <!----------Link Table---------->
-    <table align="center">
-        <tr>
-
-        </tr>
-
-
-    </table>
-    <!----------Content Table---------->
-    <table align="center">
-  <tr>
-    <td>
-        <h2>CEO: </h2>
-      <img src="image1.jpg" alt="Image 1">
-      <p>Description for image 1</p>
-    </td>
-    <td>
-    <h2>Technical Director: </h2>
-      <img src="image2.jpg" alt="Image 2">
-      <p>Description for image 2</p>
-    </td>
-    <td>
-    <h2> </h2>
-      <img src="image3.jpg" alt="Image 3">
-      <p>Description for image 3</p>
-    </td>
-    <td>
-    <h2>Marketing Manager: </h2>
-      <img src="image4.jpg" alt="Image 4">
-      <p>Description for image 4</p>
-    </td>
-  </tr>
-</table>
-
   
     
 <?php
